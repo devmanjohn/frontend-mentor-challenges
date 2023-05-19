249d3dfa25fec3f1c40d26e81235e4b2c905eeab
John Phillips <john@devmanjohn.com>
2023-05-19 13:06:35 +0100

    header cleanup

--- intermediate/minimalist-portfolio-website/components/Header.jsx
+++ intermediate/minimalist-portfolio-website/components/Header.jsx
@@ -1,16 +1,21 @@
 'use client';
 
+// Imports
+import Link from 'next/link';
 import Image from 'next/image';
 
+// State
 import { useState } from 'react';
+
+// Components
 import NavLink from './utils/NavLink';
-import Link from 'next/link';
 
 function Header() {
   const [isNavClosed, setIsNavClosed] = useState(true);
 
   return (
     <header className='custom-width py-6 border-b flex items-center justify-between relative md:border-0 md:py-14 '>
+      {/* Logo */}
       <Link href='/'>
         <Image
           src='./assets/images/logo.svg'
@@ -19,15 +24,18 @@ function Header() {
           height={32}
         />
       </Link>
-      <nav className='hidden md:flex gap-10 '>
+
+      {/* Desktop Navigation */}
+      <nav className='hidden md:flex gap-10'>
         <NavLink href='/'>Home</NavLink>
         <NavLink href='/portfolio'>Portfolio</NavLink>
         <NavLink href='/contact'>Contact Me</NavLink>
       </nav>
 
+      {/* Hamburger */}
       <button
-        className='md:hidden'
         type='button'
+        className='md:hidden'
         onClick={() => {
           setIsNavClosed((prev) => {
             setIsNavClosed(!prev);
@@ -46,6 +54,7 @@ function Header() {
         />
       </button>
 
+      {/* Mobile Navigation */}
       <nav
         className={`md:hidden absolute right-0 top-16 bg-dark-blue text-white flex gap-6 flex-col justify-center text-center uppercase tracking-[2px] w-[223px] overflow-hidden transition-all duration-300 ${
           isNavClosed ? 'h-[0px]' : 'h-[186px]'
