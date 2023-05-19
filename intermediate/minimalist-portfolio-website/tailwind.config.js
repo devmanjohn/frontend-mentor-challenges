16337db2448386dbd5fe29b5f11245138ed69c16
John Phillips <john@devmanjohn.com>
2023-05-19 10:56:47 +0100

    variable setup

--- intermediate/minimalist-portfolio-website/tailwind.config.js
+++ intermediate/minimalist-portfolio-website/tailwind.config.js
@@ -7,12 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'slightly-desaturated-cyan': '#5FB4A2',
+        'dark-blue': '#203A4C',
+        'grayish-dark-blue': '#33323D',
+        'very-light-gray': '#FAFAFA',
+        'light-gray': '#EAEAEB',
+        'bright-red': '#F43030',
+      },
+      fontFamily: {
+        ibarra: ['Ibarra Real Nova', 'serif'],
+        'public-sans': ['Public Sans', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+};
