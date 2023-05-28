5f7896dc78136e308e597f1bd6363e9826532b16
John Phillips <john@devmanjohn.com>
2023-05-28 19:54:34 +0100

    moved to main jobs, button working

---
+++ intermediate/devjobs-web-app/app/jobs/page.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import JobCard from '@/components/JobCard';
+import { db } from '@/firebase/firebaseConfig';
+import Job from '@/models/Job';
+import { useEffect, useState } from 'react';
+import {
+  collection,
+  getCountFromServer,
+  getDocs,
+  limit,
+  query,
+} from 'firebase/firestore';
+
+export default function Home() {
+  const [jobsData, setJobsData] = useState<Job[]>();
+  const [jobLimit, setJobLimit] = useState(12);
+  const [totalDocs, setTotalDocs] = useState(0);
+
+  // Query collection data for filters
+  const q = query(collection(db, 'jobs'), limit(jobLimit));
+
+  // Get collection data from firestore
+  const getJobsCollection = async () => {
+    const querySnapshot = await getDocs(q);
+    const snapshotCount = await getCountFromServer(
+      query(collection(db, 'jobs'))
+    );
+    const newData: any = querySnapshot.docs.map((doc) => ({ ...doc.data() }));
+
+    setTotalDocs(snapshotCount.data().count);
+    setJobsData(newData);
+  };
+
+  useEffect(() => {
+    getJobsCollection();
+  }, [jobLimit]);
+
+  return (
+    <main>
+      <section className='container mt-14 mb-14'>
+        <div className='grid grid-cols-1 gap-x-4 lg:gap-x-6 gap-y-14 md:grid-cols-2 lg:grid-cols-3'>
+          {jobsData?.map(
+            ({
+              id,
+              company,
+              location,
+              logo,
+              logoBackground,
+              postedAt,
+              contract,
+              position,
+            }) => {
+              return (
+                <JobCard
+                  key={id}
+                  id={id}
+                  company={company}
+                  logo={logo}
+                  logoBackground={logoBackground}
+                  location={location}
+                  postedAt={postedAt}
+                  contract={contract}
+                  position={position}
+                />
+              );
+            }
+          )}
+        </div>
+        <button
+          className='bg-[#5964E0] text-white rounded px-8 py-4 font-bold mt-12 mx-auto block disabled:opacity-10 hover:opacity-80 transition-opacity'
+          onClick={() => {
+            setJobLimit((prev) => {
+              return prev + 3;
+            });
+          }}
+          disabled={jobLimit >= totalDocs}
+        >
+          Load More
+        </button>
+      </section>
+    </main>
+  );
+}
