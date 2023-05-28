488720c419042862bf1fcc04b42d749b14f5aef3
John Phillips <john@devmanjohn.com>
2023-05-28 20:21:09 +0100

    job details setup

--- intermediate/devjobs-web-app/app/jobs/[id]/page.tsx
+++ intermediate/devjobs-web-app/app/jobs/[id]/page.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+'use client';
 
-function JobDetails() {
-  return <div>JobDetails</div>;
+import LogoBox from '@/components/utils/LogoBox';
+import { db } from '@/firebase/firebaseConfig';
+import Job from '@/models/Job';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+type JobTypes = {
+  params: { id: number };
+};
+
+function JobDetails({ params }: JobTypes) {
+  const [job, setJob] = useState<Job>();
+
+  const getJob = async () => {
+    const docRef = doc(db, 'jobs', `${params.id}`);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setJob(docSnap.data() as Job);
+    } else {
+      console.log('Document was not found!');
+    }
+  };
+
+  useEffect(() => {
+    getJob();
+  }, [params.id]);
+
+  return (
+    <main>
+      <section className='container'>
+        <p>{job?.company}</p>
+        <p>Company</p>
+      </section>
+    </main>
+  );
 }
 
 export default JobDetails;
--- intermediate/devjobs-web-app/app/page.tsx
+++ intermediate/devjobs-web-app/app/page.tsx
@@ -1,79 +1,13 @@
 'use client';
 
-import JobCard from '@/components/JobCard';
-import { db } from '@/firebase/firebaseConfig';
-import Job from '@/models/Job';
-import {
-  collection,
-  doc,
-  getDocs,
-  limit,
-  onSnapshot,
-  query,
-} from 'firebase/firestore';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
-  const [jobsData, setJobsData] = useState<Job[]>();
-  const [jobLimit, setJobLimit] = useState(12);
-
-  // Query collection data for filters
-  const q = query(collection(db, 'jobs'), limit(jobLimit));
-
-  // Get collection data from firestore
-  const getJobsCollection = async () => {
-    const querySnapshot = await getDocs(q);
-
-    const newData: any = querySnapshot.docs.map((doc) => ({ ...doc.data() }));
-    setJobsData(newData);
-  };
-
+function Home() {
+  const router = useRouter();
   useEffect(() => {
-    getJobsCollection();
-  }, [jobLimit]);
-
-  return (
-    <main>
-      <section className='container mt-14 mb-14'>
-        <div className='grid grid-cols-1 gap-x-4 lg:gap-x-6 gap-y-14 md:grid-cols-2 lg:grid-cols-3'>
-          {jobsData?.map(
-            ({
-              id,
-              company,
-              location,
-              logo,
-              logoBackground,
-              postedAt,
-              contract,
-              position,
-            }) => {
-              return (
-                <JobCard
-                  key={id}
-                  id={id}
-                  company={company}
-                  logo={logo}
-                  logoBackground={logoBackground}
-                  location={location}
-                  postedAt={postedAt}
-                  contract={contract}
-                  position={position}
-                />
-              );
-            }
-          )}
-        </div>
-        <button
-          onClick={() => {
-            setJobLimit((prev) => {
-              return prev + 3;
-            });
-          }}
-          className='bg-[#5964E0] text-white rounded px-8 py-4 font-bold mt-12 mx-auto block'
-        >
-          Load More
-        </button>
-      </section>
-    </main>
-  );
+    router.push('/jobs');
+  }, []);
 }
+
+export default Home;
