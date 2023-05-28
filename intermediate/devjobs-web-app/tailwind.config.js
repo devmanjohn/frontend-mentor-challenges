30a75aa95fdba9511327f3b9095c80ebd1871585
John Phillips <john@devmanjohn.com>
2023-05-28 14:08:50 +0100

    assets added

--- intermediate/devjobs-web-app/tailwind.config.js
+++ intermediate/devjobs-web-app/tailwind.config.js
@@ -7,12 +7,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // Normally I would theme the colours. I decided to manually do it this time! :)
     },
   },
   plugins: [],
-}
+};
